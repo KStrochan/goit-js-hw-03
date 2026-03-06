08d7348a0e2c91181b5cda33b37b2c7be44688c6
Konstantyn <72542265+KStrochan@users.noreply.github.com>
2026-03-06 20:27:11 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
-function filterArray(numbers, threshold) {
-  return numbers.filter(num => num > threshold);
+function filterArray(numbers, value) {
+  return numbers.filter(num => num > value)
 }
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
